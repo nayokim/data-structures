df8f13a3ae62d3df6ce442f297a52737d5a59713
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-08-07 16:36:21 -0500

    add to head of linked list

---
+++ src/InsertNodeAtHead/InsertNodeI.java
@@ -0,0 +1,61 @@
+package InsertNodeAtHead;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class InsertNodeI {
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    //llist == head
+    static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data) {
+        SinglyLinkedListNode node = new SinglyLinkedListNode(data);
+        if (llist == null){
+            return  node;
+        }
+
+        //
+        node.next = llist;
+        return node;
+
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
